6b2436465bb1cbd715e0a39e8a952a6264893f6c
kweenofcode <ky.capstick@gmail.com>
2018-12-13 00:59:55 -0500

    initially deploy

--- api/server.js
+++ api/server.js
@@ -2,7 +2,11 @@
 
 const bodyParser = require('body-parser');
 const express = require('express');
-
+const path = require('path')
+const config = require('config');
+const mongoose = require('mongoose')
+const API_PORT = process.env.PORT || config.PORT;
+const MONGOOSE_URI = process.env.MONGODB_URI || config.MONGODB_URI;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,6 +18,11 @@ app.use('/users', require('./routes/users'))
 app.use('/login', require('./routes/login'))
 app.use('/game', require('./routes/game'))
 app.use('/signup', require('./routes/signup'))
+app.use('/', express.static(path.join(__dirname, '../build')))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'))
+})
+
 
 app.use((err, req, res, next) => {
   if (err.name === 'UnauthorizedError') {
@@ -24,5 +33,10 @@ app.use((err, req, res, next) => {
     res.status(401).json({ errors });
   }
 });
+
+app.listen(API_PORT, () => {
+  console.log(`Listening on Port ${API_PORT}`)
+})
+mongoose.connect(MONGOOSE_URI)
 
 module.exports = app;
