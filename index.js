7bc5bd9bb28cb55a776d5249aaaf5f796c45da75
kweenofcode <ky.capstick@gmail.com>
2018-12-13 08:36:48 -0500

    initially deploy

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-'use strict';
+// 'use strict';
 
-const server = require('./api/server');
-const { PORT, DB_URI } = require('./api/utils/constants');
-const mongoose = require('mongoose')
+// const server = require('./api/server');
+// const { PORT, DB_URI } = require('./api/utils/constants');
+// const mongoose = require('mongoose')
 
-server.listen(PORT, async () => {
-  await mongoose.connect(DB_URI)
-  console.log(`Database connected at ${DB_URI}`)
-  console.log(`App listening on port ${PORT}`)
-});
+// server.listen(PORT, async () => {
+//   await mongoose.connect(DB_URI)
+//   console.log(`Database connected at ${DB_URI}`)
+//   console.log(`App listening on port ${PORT}`)
+// });
